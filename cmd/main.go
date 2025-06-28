07f34909d85336dc77643fec8ea18af1ef3ac2d5
Pinghao Wu <xdavidwuph@gmail.com>
2025-06-28 14:38:44 +0800

    main: support kubectl plugin completion

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"os"
+	"path"
+
+	"github.com/spf13/cobra"
+)
+
 func main() {
+	if path.Base(os.Args[0]) == "kubectl_complete-mutated" {
+		mutatedCmd.SetArgs(append([]string{cobra.ShellCompRequestCmd}, os.Args[1:]...))
+	}
 	mutatedCmd.Execute()
 }
