1d059442561e8adc46c752d4101205d8201586ac
Pinghao Wu <xdavidwuph@gmail.com>
2025-07-12 10:32:05 +0800

    metadata: managed_fields: catch helm
    See: #3

--- internal/metadata/managed_fields.go
+++ internal/metadata/managed_fields.go
@@ -4,6 +4,14 @@ import (
 	"strings"
 )
 
+// Either explicitly specified, or from user-agent before '/'
+// see k8s.io/apiserver/pkg/endpoints/handlers.managerOrUserAgent
 func IsManualManager(m string) bool {
-	return strings.HasPrefix(m, "kubectl") && m != "kubectl-rollout"
+	return (strings.HasPrefix(m, "kubectl") && m != "kubectl-rollout") ||
+		// helm cli managed resources (via its generic client)
+		// (flux helm-controller uses "helm-controller")
+		m == "helm" ||
+		// helm cli storage (secrets, configmaps) implicitly via user-agent Helm/<version>
+		// (flux helm-controller users "helm-controller")
+		m == "Helm"
 }
